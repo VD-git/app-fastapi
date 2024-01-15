591bc7977ae6c5da91f1fef16833e83396239ef0
Victor Dias <victordias94@hotmail.com>
2024-01-15 02:14:03 -0300

    API Built

--- starter/main.py
+++ starter/main.py
@@ -1,6 +1,9 @@
 # Put the code for your API here.
 from fastapi import FastAPI
-from typing import Union 
+from lightgbm import Booster
+from starter.ml.data import process_data
+from starter.ml.model import inference
+import pickle
 from pydantic import BaseModel
 import pandas as pd
 import os
@@ -27,18 +30,52 @@ app = FastAPI()
 async def say_hello():
     return {"greeting": "Hello World!"}
 
-# @app.post("/prediction/")
-# async def output(payload: PayloadItem):
-#     os.chdir('/home/vdias94/app-fastapi/starter')
-    
-#     data = pd.DataFrame(payload)
-#     with open(encoder_path, "rb") as f: 
-#         encoder = pickle.load(f)
-#     with open(lb_path, "rb") as f: 
-#         lb = pickle.load(f)
-#     model = Booster(model_file=model_path)
+@app.post("/prediction/")
+async def output(payload: PayloadItem):
 
-#     y_pred = inference(model, X_test)
+    # Building path to be imported
+    encoder_path = os.path.join(os.getcwd(), 'model', 'encoder')
+    lb_path = os.path.join(os.getcwd(), 'model', 'lb')
+    model_path = os.path.join(os.getcwd(), 'model', 'model.txt')
+    json_file = {
+        "age": payload.age,
+        "workclass": payload.workclass,
+        "fnlgt": payload.fnlgt,
+        "education": payload.education,
+        "education-num": payload.educationnum,
+        "marital-status": payload.maritalstatus,
+        "occupation": payload.occupation,
+        "relationship": payload.relationship,
+        "race": payload.race,
+        "sex": payload.sex,
+        "capital-gain": payload.capitalgain,
+        "capital-loss": payload.capitalloss,
+        "hours-per-week": payload.hoursperweek,
+        "native-country": payload.nativecountry
+    }
+
+    # Creating DataFrame to run the model with
+    data = pd.DataFrame([json_file])
+    
+    # Importing models
+    with open(encoder_path, "rb") as f: 
+        encoder = pickle.load(f)
+    with open(lb_path, "rb") as f: 
+        lb = pickle.load(f)
+    model = Booster(model_file=model_path)
+    
+    cat_features = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "native-country"]
+
+    # Preprocessing with the encoder
+    X, _, _, _ = process_data(
+        data, categorical_features=cat_features, label=None, training=False,
+        encoder=encoder, lb=lb
+    )
+
+    # Building the inference model
+    prediction = inference(model, X)
+
+    return {"response": int(prediction)}
     
     
     
