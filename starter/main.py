433558cceaa986b2999705d0a29110640aa45b18
Victor Dias <victordias94@hotmail.com>
2024-01-14 22:26:14 -0300

    Start API Aplication in main.py

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,54 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from typing import Union 
+from pydantic import BaseModel
+import pandas as pd
+
+class PayloadItem(BaseModel):
+    age: int 
+    workclass: str
+    fnlgt: int 
+    education: str
+    education-num: int 
+    marital-status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital-gain: int 
+    capital-loss: int 
+    hours-per-week: int 
+    native-country: str
+
+app = FastAPI()
+
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello World!"}
+
+@app.post("/prediction/")
+async def output(payload: PayloadItem):
+    data = pd.DataFrame(payload)
+    with open(encoder_path, "rb") as f: 
+        encoder = pickle.load(f)
+    with open(lb_path, "rb") as f: 
+        lb = pickle.load(f)
+    model = Booster(model_file=model_path)
+
+# Perform analysis of the metrics of the model, dump a json for future analysis
+y_pred = inference(model, X_test)
+    
+    
+    
+
+
+     lb = pickle.load(f)
+    model = Booster(model_file=model_path)
+
+# Perform analysis of the metrics of the model, dump a json for future analysis
+y_pred = inference(model, X_test)
+    
+    
+    
+
+
