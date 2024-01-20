b40bdbf4b1d34dc1cef7c36cf7a1b0b1f30e5348
Victor Dias <victordias94@hotmail.com>
2024-01-20 20:38:05 -0300

    Update of Live Post Images and Script

---
+++ starter/post_method.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import argparse
+import requests
+import json
+
+def go(args):
+    r = requests.post(args.url, args.payload)
+    status, response = r.status_code, r.json()
+    return {"status": status, "response": response.get("response"), "payload": args.payload}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Testing POST Method API"
+    )
+
+    parser.add_argument(
+        "--url",
+        type=str,
+        help="Render URL used for the API",
+        required=False,
+        default="https://fastapi-application-gl4h.onrender.com/prediction"
+    )
+
+    parser.add_argument(
+        "--payload",
+        type=str,
+        help="Root for the names of the produced artifacts. The script will produce 2 artifacts: "
+             "{root}_train.csv and {root}_test.csv",
+        required=False,
+        default='{"age": 39, "workclass": "State-gov", "fnlgt": 77516, "education": "Bachelors", "education-num": 13, "marital-status": "Never-married", "occupation": "Adm-clerical", "relationship": "Not-in-family", "race": "White", "sex": "Male", "capital-gain": 21740, "capital-loss": 0, "hours-per-week": 40, "native-country": "United-States"}'
+    )
+
+    args = parser.parse_args()
+
+    print(go(args))
